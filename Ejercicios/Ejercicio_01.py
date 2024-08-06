6a57cde130f23df4b38ec86017962a8d64173b86
Lucas <81052967+lucas-oliaro@users.noreply.github.com>
2024-08-06 19:36:27 -0300

    To Fix: @Classmethod

--- Ejercicios/Ejercicio_01.py
+++ Ejercicios/Ejercicio_01.py
@@ -1,4 +1,4 @@
-class Lamparita():
+class Lampara():
 
     def __init__(self) -> None:
         self.estado = False
@@ -12,13 +12,30 @@ class Lamparita():
     def mostrar_estado(self):
        print("Apagado" if self.estado==False else "Prendido")
               
+    #@classmethod
+    #def seCortoLaLuz(cls):
+        #Ver
+
+
 if __name__ == '__main__':
-    lamparita = Lamparita()
+    print("Lampara ")
+    lamparita = Lampara()
     lamparita.mostrar_estado()
     lamparita.prender()
     lamparita.mostrar_estado()
     lamparita.apagar()
     lamparita.mostrar_estado()
+    print("Lampara 2")
     
+    lamparita1 = Lampara()
+    lamparita1.mostrar_estado()
+    lamparita1.prender()
+    lamparita1.mostrar_estado()
+
+
+    print("Se apagan ambas")
+    #Lampara.seCortoLaLuz()
+    #lamparita.mostrar_estado()
+    #lamparita1.mostrar_estado()
 
 
