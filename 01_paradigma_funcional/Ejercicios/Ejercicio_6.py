0134c619e32d64fd3524b1839b847a652904ad2d
Lucas <81052967+lucas-oliaro@users.noreply.github.com>
2024-08-30 09:18:45 -0300

    Consigna corrected

--- 01_paradigma_funcional/Ejercicios/Ejercicio_6.py
+++ 01_paradigma_funcional/Ejercicios/Ejercicio_6.py
@@ -1,4 +1,6 @@
 # Ejercicio: Registrando logs
-# A lo largo de nuestro programa es posible que necesitemos almacenar información de interés en el log de ejecución. A efectos prácticos, nuestro destino de log será la consola, por lo que podemos utilizar simplemente un print() para registrar un mensaje de log.
+# A lo largo de nuestro programa es posible que necesitemos almacenar información de interés en el log de ejecución. 
+# A efectos prácticos, nuestro destino de log será la consola, por lo que podemos utilizar simplemente un print()
+#  para registrar un mensaje de log.
 
 # Implementar una función log currificada que permita registrar un mensaje de log y el tipo, que puede ser error, alerta o información.
