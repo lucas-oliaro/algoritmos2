877cc4559a60ad32b5821bbabf9bd2e8cff1bfc8
Lucas <81052967+lucas-oliaro@users.noreply.github.com>
2024-09-10 18:35:32 -0300

    Resuelto

--- 01_paradigma_funcional/Ejercicios/Ejercicio_9.py
+++ 01_paradigma_funcional/Ejercicios/Ejercicio_9.py
@@ -4,3 +4,33 @@
 # los valores sean la cantidad de apariciones que tiene ese elemento en la lista.
 
 # Ejemplo: contar(['a', 'b', 'c', 'a', 'a', 'c', 'b', 'd', 'c', 'a', 'e']) -> {'a': 4, 'b': 2, 'c': 3, 'd': 1, 'e': 1}.
+
+#%% 
+# Idea personal
+def contar(lista : list) -> dict:
+    result = dict(zip(lista,map(lambda x: lista.count(x),lista)))
+    return result
+
+
+
+if __name__ == "__main__":
+    print(contar(['a', 'a', 'a'])) #-> {a : 3}
+    print(contar(['a', 'b', 'c', 'a', 'a', 'c', 'b', 'd', 'c', 'a', 'e']))
+
+
+
+
+# %%
+
+#Solution GPT
+from collections import Counter
+
+def contar2(lista: list) -> dict:
+    return dict(Counter(lista))
+
+if __name__ == "__main__":
+    print(contar2(['a', 'a', 'a'])) #-> {a : 3}
+    print(contar2(['a', 'b', 'c', 'a', 'a', 'c', 'b', 'd', 'c', 'a', 'e']))
+
+
+# %%
