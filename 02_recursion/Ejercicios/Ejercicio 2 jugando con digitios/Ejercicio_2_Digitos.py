b0ac7b40f4bddca1b1e24833ec40aeb17798434c
Lucas <81052967+lucas-oliaro@users.noreply.github.com>
2024-09-12 16:44:01 -0300

    #TODO

---
+++ 02_recursion/Ejercicios/Ejercicio 2 jugando con digitios/Ejercicio_2_Digitos.py
@@ -0,0 +1,7 @@
+#jercicio: Jugando con dígitos
+# Definir lo siguiente:
+
+# a. Una función recursiva digitos, que dado un número entero, retorne su cantidad de dígitos.
+# b. Una función recursiva reversa_num que, dado un número entero, retorne su imagen especular. Por ejemplo: reversa_num(345) = 543
+# c. Una función recursiva suma_digitos que, dado un número entero, retorne la suma de sus dígitos.
+# d. Una función recursiva que retorne los dos valores anteriores a la vez como un par, aprovechando la recursión.
